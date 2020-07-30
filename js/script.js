f21bb1fbdbeeb60cbc57debe186cae3020d11ce7
AlejandraGiraldo <egiraldoe@iberoamericana.edu.co>
2020-07-30 18:06:38 -0500

    Actividad 7

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function nombreSitioWeb() {
+
+    var nombreSitioWeb = prompt ("¿Esta pagina es educativa?"); 
+
+
+switch (nombreSitioWeb) {
+    case "EDUCACION":
+        alert("El nombre es correcto");
+        break;
+
+    default:
+        alert("El nombre es incorrecto");
+        break;
+}
+
+}
+function confirmacion(){
+
+    confirm("EL NOMBRE DE LA PAGINA ES EDUCACION");
+}
+
+function nombreSitioWeb1() {
+
+    var nombreSitioWeb1 = prompt ("¿TE FUE UTIL LA INFORMACION?");
+
+    switch (nombreSitioWeb1) {
+        case "si":
+            alert("El nombre es correcto");
+            break;
+    
+        default:
+            alert("El nombre es incorrecto");
+            break;
+    }
+
+
+
+    switch (nombreSitioWeb2) {
+        case "si":
+            alert("El nombre es correcto");
+            break;
+    
+        default:
+            alert("El nombre es incorrecto");
+            break;
+    }
+}
+
